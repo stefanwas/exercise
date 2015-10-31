85eb916e0334a5a57c7c50af6483fae93d2f327c
stefanwas <swtemp001@gmail.com>
2015-10-31 23:28:14 +0100

    cleanup

--- src/main/java/base/configuration/ApplicationConfig.java
+++ src/main/java/base/configuration/ApplicationConfig.java
@@ -1,21 +1,11 @@
 package base.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
-import org.springframework.web.servlet.view.xml.MarshallingView;
-
-import java.util.Arrays;
-
-import javax.servlet.ServletContext;
 
 
 /**
